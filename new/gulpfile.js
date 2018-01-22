54c3f239a480d82709cca23977f7c494bae08575
fonzk <alfdiazr@hotmail.com>
2018-01-22 18:31:47 -0500

    empezando

--- new/gulpfile.js
+++ new/gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp')
 var postcss = require('gulp-postcss')
+var autoprefixer = require('autoprefixer')
+var cssnested = require('postcss-nested')
 var browserSync = require('browser-sync').create()
-
-
 // servidor de desarrollo
 
 gulp.task('serve',function(){
@@ -19,7 +19,11 @@ server: {
 // tarea para procesar el CSS
 
 gulp.task('css',function(){
-var processors =[]
+var processors =[
+autoprefixer({browsers:['>50%','ie 8']}),
+cssnested
+
+]
 return gulp.src('./src/*.css')
 .pipe(postcss(processors)) 
 .pipe(gulp.dest('./dist/css'))
